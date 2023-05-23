58f9933bff743be01856a415f7caf40768b552de
soni895 <darshansoni895@gmail.com>
2023-05-23 18:01:02 +0530

    remove event listner

--- index.js
+++ index.js
@@ -2,13 +2,52 @@
 
 // fatch element by id
 
-function print()
+print=function ()
 {
     console.log("event listner hare") ;
 }
-let heading = document.querySelector('h1');
-console.log(heading);
-document.addEventListener('click',function()
+// let heading = document.querySelector('h1');
+// heading.log(heading);
+
+// way 1
+
+// document.addEventListener('click',function()
+// {
+//     print();
+// });
+
+
+// way2
+// document.addEventListener('click',function()
+// {
+//     console.log("hi darshan soni");
+// });
+
+
+// way3
+document.addEventListener('click',print);
+// var classname = document.getElementsByClassName("name");
+
+// var myFunction = function() {
+//     var attribute = this.getAttribute("data-myattribute");
+//     alert(attribute);
+// };
+
+// classname.addEventListener('click', myFunction, false);
+
+
+function print()
 {
-    print();
-});
+
+    console.log("hi darshan soni");
+}
+// const content=document.querySelector(".Name");
+// content.addEventListener('click',funtion(event)
+// {
+//     console.log(event);
+// });
+
+// remove listner 
+
+document.removeEventListener('click',print);
+
