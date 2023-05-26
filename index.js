cc96d8c617a439bfa1daeda7c487c65fec236433
soni895 <darshansoni895@gmail.com>
2023-05-26 16:25:30 +0530

    optimized

--- index.js
+++ index.js
@@ -132,26 +132,27 @@ document.body.appendChild(mydiv);
 
 // speed mesure
 
-// let t1=performance.now();
-// for(let i=0;i<100;i++)
-// {
-//     let para=document.createElement('p');
-//     para.textContent="hi darshan soni"+1;
-//     document.body.appendChild(para);
-// }
-// let t2=performance.now();
-// console.log(t2-t1);
+let t1=performance.now();
+for(let i=0;i<100;i++)
+{
+    let para=document.createElement('p');
+    para.textContent="hi darshan soni"+1;
+    document.body.appendChild(para);
+}
+let t2=performance.now();
+console.log(t2-t1);
 
-// let div=document.createElement('div');
-// console.log("little optimize");
-// let t3=performance.now();
-// for(let i=0;i<100;i++)
-// {
-//     let para=document.createElement('p');
-//     para.textContent="hi darshan soni"+1;
-//     div.appendChild(para);
+let div=document.createElement('div');
+console.log("little optimize");
+let t3=performance.now();
+for(let i=0;i<100;i++)
+{
+    let para=document.createElement('p');
+    para.textContent="hi darshan soni"+1;
+    div.appendChild(para);
     
-// }
-// document.body.appendChild(div);
-// let t4=performance.now();
-// console.log(t3-t4);
+}
+document.body.appendChild(div);
+let t4=performance.now();
+console.log(t4-t3);
+
